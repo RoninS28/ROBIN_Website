c0920d1a6f5e017dcdb35ed028882133464510ac
pratham022 <pthombre657@gmail.com>
2021-10-25 23:22:26 +0530

    Add Button to Order Receipt Page

--- client/src/superAdmin/Pages/AccessoryOrderReceipt.js
+++ client/src/superAdmin/Pages/AccessoryOrderReceipt.js
@@ -3,8 +3,8 @@ import PrintIcon from '@material-ui/icons/Print';
 import { Grid, IconButton } from '@mui/material';
 // import { SaveAlt } from '@material-ui/icons';
 import SaveAltIcon from '@material-ui/icons/SaveAlt'
-import { Typography } from "@material-ui/core";
-import {Card} from '@material-ui/core';
+import { Button, Typography } from "@material-ui/core";
+import { Card } from '@material-ui/core';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -28,14 +28,14 @@ const styles = theme => ({
 
 //Details table
 function createData(property, data) {
-    return { property, data};
+    return { property, data };
 }
 
 
 
 // for the 1st grid
 const rows = [
-    createData('Order ID','C2K5464'),
+    createData('Order ID', 'C2K5464'),
     createData('Factory', 'Noga Factory'),
     createData('Date', '10 Oct 2021'),
     createData('Status', 'Pending'),
@@ -45,21 +45,21 @@ const rows = [
 const TAX_RATE = 0.15;
 
 function ccyFormat(num) {
-  return `${num.toFixed(2)}` ;
+    return `${num.toFixed(2)}`;
 }
 
 function createRowService(id, name, quantity, pricePerUnit, price) {
-  return { id, name, quantity, pricePerUnit, price };
+    return { id, name, quantity, pricePerUnit, price };
 }
 
 function subtotal(items) {
-  return items.map(({ price }) => price).reduce((sum, i) => sum + i, 0);
+    return items.map(({ price }) => price).reduce((sum, i) => sum + i, 0);
 }
 
 const rowService = [
-  createRowService(1, 'PU Leather Seat', 10, 3200, 32000),
-  createRowService(2, 'PU Leather Seat', 100, 3200, 320000),
-  createRowService(3, 'PU Leather Seat', 10, 3200, 32000),
+    createRowService(1, 'PU Leather Seat', 10, 3200, 32000),
+    createRowService(2, 'PU Leather Seat', 100, 3200, 320000),
+    createRowService(3, 'PU Leather Seat', 10, 3200, 32000),
 ];
 
 const invoiceSubtotal = subtotal(rowService);
@@ -68,15 +68,15 @@ const invoiceTotal = invoiceTaxes + invoiceSubtotal;
 
 const AccessoryOrderReceipt = (props) => {
 
-    const {classes, theme} = props;
+    const { classes, theme } = props;
 
     return (
-        <div style={{padding:'1rem'}}>
-            <Grid container 
-            justifyContent="center">
+        <div style={{ padding: '1rem' }}>
+            <Grid container
+                justifyContent="center">
                 <Grid container item xs={12}
                     justifyContent="right">
-                   <Grid item>
+                    <Grid item>
                         <IconButton>
                             <SaveAltIcon></SaveAltIcon>
                         </IconButton>
@@ -89,80 +89,84 @@ const AccessoryOrderReceipt = (props) => {
                 </Grid>
 
                 <Grid container
-                justifyContent='center'
-                spacing='1rem'
-                alignItems="center"          
-                xs={10}>
+                    justifyContent='center'
+                    spacing='1rem'
+                    alignItems="center"
+                    xs={10}>
                     <Grid item xs={12}>
                         <Card elevation={3} >
                             <TableContainer >
-                                <Table  aria-label="Details">
+                                <Table aria-label="Details">
                                     <TableBody>
                                         {rows.map((row) => (
                                             <TableRow
-                                            key={row.property}
-                                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                                                key={row.property}
+                                                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                                             >
                                                 <TableCell component="th" scope="row" align="center"><b>
-                                                {row.property} </b>
+                                                    {row.property} </b>
                                                 </TableCell>
                                                 <TableCell align="center">{row.data}</TableCell>
                                             </TableRow>
                                         ))}
                                     </TableBody>
                                 </Table>
-                            </TableContainer>                    
+                            </TableContainer>
                         </Card>
                     </Grid>
                     <Grid item xs={12}>
                         <Card elevation={3} >
-                        <TableContainer>
-                            <Table sx={{ minWidth: 700 }} aria-label="spanning table">
-                                <TableHead>
-                                <TableRow>
-                                    <TableCell align="center" colSpan={3}><b>
-                                    ORDER DETAILS</b>
-                                    </TableCell> 
-                                    <TableCell align="right"><b>PRICE (₹)</b></TableCell>
-                                </TableRow>
-                                <TableRow>
-                                    <TableCell><b>NAME</b></TableCell>
-                                    <TableCell align="right"><b>QUANTITY</b></TableCell>
-                                    <TableCell align="right"><b>PRICE per ITEM</b></TableCell>
-                                    <TableCell align="right"><b>SUM</b></TableCell>
-                                </TableRow>
-                                </TableHead>
-                                <TableBody>
-                                {rowService.map((rowService) => (
-                                    <TableRow key={rowService.id}>
-                                    <TableCell>{rowService.name}</TableCell>
-                                    <TableCell align="right">{rowService.quantity}</TableCell>
-                                    <TableCell align="right">{rowService.pricePerUnit}</TableCell>
-                                    <TableCell align="right">{ccyFormat(rowService.price)}</TableCell>
-                                    </TableRow>
-                                ))}
+                            <TableContainer>
+                                <Table sx={{ minWidth: 700 }} aria-label="spanning table">
+                                    <TableHead>
+                                        <TableRow>
+                                            <TableCell align="center" colSpan={3}><b>
+                                                ORDER DETAILS</b>
+                                            </TableCell>
+                                            <TableCell align="right"><b>PRICE (₹)</b></TableCell>
+                                        </TableRow>
+                                        <TableRow>
+                                            <TableCell><b>NAME</b></TableCell>
+                                            <TableCell align="right"><b>QUANTITY</b></TableCell>
+                                            <TableCell align="right"><b>PRICE per ITEM</b></TableCell>
+                                            <TableCell align="right"><b>SUM</b></TableCell>
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                        {rowService.map((rowService) => (
+                                            <TableRow key={rowService.id}>
+                                                <TableCell>{rowService.name}</TableCell>
+                                                <TableCell align="right">{rowService.quantity}</TableCell>
+                                                <TableCell align="right">{rowService.pricePerUnit}</TableCell>
+                                                <TableCell align="right">{ccyFormat(rowService.price)}</TableCell>
+                                            </TableRow>
+                                        ))}
 
-                                <TableRow>
-                                    <TableCell rowSpan={3} />
-                                    <TableCell colSpan={2}><b>Subtotal</b></TableCell>
-                                    <TableCell align="right">{ccyFormat(invoiceSubtotal)}</TableCell>
-                                </TableRow>
-                                <TableRow>
-                                    <TableCell><b>Tax</b></TableCell>
-                                    <TableCell align="right">{`${(TAX_RATE * 100).toFixed(0)} %`}</TableCell>
-                                    <TableCell align="right">{ccyFormat(invoiceTaxes)}</TableCell>
-                                </TableRow>
-                                <TableRow>
-                                    <TableCell colSpan={2}><b>Total</b></TableCell>
-                                    <TableCell align="right">{ccyFormat(invoiceTotal)}</TableCell>
-                                </TableRow>
-                                </TableBody>
-                            </Table>
-                        </TableContainer>            
+                                        <TableRow>
+                                            <TableCell rowSpan={3} />
+                                            <TableCell colSpan={2}><b>Subtotal</b></TableCell>
+                                            <TableCell align="right">{ccyFormat(invoiceSubtotal)}</TableCell>
+                                        </TableRow>
+                                        <TableRow>
+                                            <TableCell><b>Tax</b></TableCell>
+                                            <TableCell align="right">{`${(TAX_RATE * 100).toFixed(0)} %`}</TableCell>
+                                            <TableCell align="right">{ccyFormat(invoiceTaxes)}</TableCell>
+                                        </TableRow>
+                                        <TableRow>
+                                            <TableCell colSpan={2}><b>Total</b></TableCell>
+                                            <TableCell align="right">{ccyFormat(invoiceTotal)}</TableCell>
+                                        </TableRow>
+                                    </TableBody>
+                                </Table>
+                            </TableContainer>
                         </Card>
                     </Grid>
                 </Grid>
             </Grid>
+            <div className={classes.btnContainer}>
+                    <Button className={classes.btn} variant="contained" style={{ marginTop: "3rem", marginBottom: "1rem" }} color="secondary">Deny</Button>
+                    <Button className={classes.btn} variant="contained" style={{ marginTop: "3rem", marginBottom: "1rem" }} color="primary">Approve</Button>
+            </div>
         </div>
     )
 }
