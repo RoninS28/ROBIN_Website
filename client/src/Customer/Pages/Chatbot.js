b5ff5e6b196118bc7f75afd2b3ee721de5530f1e
Rohan Shivesh <rvss28@gmail.com>
2021-10-28 02:33:27 +0530

    add chatbot. rem-category popup

---
+++ client/src/Customer/Pages/Chatbot.js
@@ -0,0 +1,85 @@
+import React, { useRef, useState } from "react";
+import '../PagesStyles/Chatbot.css'
+import ChatMessage from "./ChatMessageModel";
+import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
+import SendIcon from '@mui/icons-material/Send';
+
+const ChatRoom = () => {
+    const dummy = useRef()
+
+    const [messages, setMessages] = useState([
+        {
+            id: '1',
+            sender: 'user',
+            text: 'My EV is not switching on. Tried jumpstarting it but to no avail',
+            timestamp: '09/09/2021 9:20am',
+            trailers: [
+                'Motor Issue'
+            ]
+        },
+        {
+            id: '2',
+            sender: 'bot',
+            text: 'Try switching it off, and putting the choke. Was this helpful?',
+            timestamp: '09/09/2021 9:20am',
+            trailers: [
+                'Yes',
+                'No',
+                'Need technical assist'
+            ]
+        },
+    ]
+    )
+    const [formValue, setFormValue] = useState('')
+
+
+    const sendMessage = async (e) => {
+        e.preventDefault();
+        if (formValue != '') {
+
+            setMessages(messages => messages.concat({
+                id: '3',
+                sender: 'user',
+                text: formValue,
+                timestamp: '09/09/2021 9:20am',
+                trailers: [
+
+                ]
+            }))
+
+            setFormValue('')
+            dummy.current.scrollIntoView({ behavior: 'smooth' })
+        }
+    }
+
+    const avoidRefresh = (e) => {
+        e.preventDefault();
+    }
+
+    return (
+        <div >
+
+            <div className="header" >
+                SMART CHATBOT
+            </div>
+
+
+            <main>
+
+                {messages && messages.map(msg => <ChatMessage key={msg.id} msg={msg} />)}
+                <div ref={dummy}></div>
+            </main>
+            <div style={{ display: 'flex', justifyContent: 'center' }}>
+
+                <form onSubmit={avoidRefresh} >
+
+                    <input type="text" value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="Write your response" />
+                    <button >Category</button>
+                    <button type="submit" onClick={sendMessage}><SendIcon style={{ color: '#4EBCEC' }} /></button>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+export default ChatRoom;
