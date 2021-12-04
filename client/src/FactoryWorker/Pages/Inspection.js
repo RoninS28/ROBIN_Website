29ff4ddb653221f8c3267c4db37fb4a374ea26f1
icon <srramane6@gmail.com>
2021-12-04 13:59:29 +0530

    Page Header

--- client/src/FactoryWorker/Pages/Inspection.js
+++ client/src/FactoryWorker/Pages/Inspection.js
@@ -1,54 +1,34 @@
-import React from 'react'
-import PropTypes from 'prop-types';
-import { useTheme } from '@material-ui/core/styles';
-import Box from '@material-ui/core/Box';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableFooter from '@material-ui/core/TableFooter';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
-import TableHead from '@material-ui/core/TableHead';
-import Paper from '@material-ui/core/Paper';
-import IconButton from '@material-ui/core/IconButton';
-import FirstPageIcon from '@material-ui/icons/FirstPage';
-import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
-import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
-import LastPageIcon from '@material-ui/icons/LastPage';
-import Button from '@material-ui/core/Button';
-import DeleteIcon from '@material-ui/icons/Delete';
-import EditIcon from '@material-ui/icons/Edit';
-import Container from '@material-ui/core/Container';
+import React, { useState } from 'react'
+
+import PageHeader from "./PageHeader";
+import TwoWheelerIcon from '@mui/icons-material/TwoWheeler';
+import {makeStyles} from '@material-ui/core';
 
 
-import { withStyles } from "@material-ui/core/styles";
-import { useMediaQuery } from '@material-ui/core';
-
-
-const styles = theme => ({
-
-    listWrapper: {
-        display: "flex",
-        flexDirection: "column",
-        marginBottom: "1rem"
+const useStyle = makeStyles(theme => ({
+    pageContent: {
+        margin: theme.spacing(2),
+        padding: theme.spacing(2)
     },
-    topRow: {
-        display: "flex",
-        justifyContent: "flex-end",
+    searchInput: {
+        width: '75%'
     },
-    rowHeader: {
-        fontWeight: "bold !important",
+    newButton: {
+        position: 'absolute',
+        right: '10px'
     }
-    
+}))
 
-})
-function Inspection()
-{
-    return(
-        <div>
-            <h1>In Inspection hello</h1>
-        </div>
-    );
+
+export default function Inspection() {
+
+    return (
+       
+            <PageHeader
+                title="Vehicles' Inspection"
+                subTitle="Manufacturing in factory"
+                icon={<TwoWheelerIcon fontSize="large" />}
+            />
+           
+    )
 }
-export default Inspection;
