3546bbf4b85e5722de7ebd03f9887628f6eaa216
tanaya123 <tanayawankar58@gmail.com>
2022-03-29 22:22:08 +0530

    Small changes

--- app.js
+++ app.js
@@ -49,23 +49,18 @@ app.use('/model',require('./routes/common/model'));
 // app.use('/customers', require('./routes/customer/customer'));
 
 // ! CUSTOMER ROUTER
-app.use(custAuthRoutes)
-app.use('/products', requireCustAuth, require('./routes/customer/products'));
-app.use('/servicing', requireCustAuth, require('./routes/customer/servicing'));
-app.use('/chatbot', requireCustAuth, require('./routes/customer/chatbotMsg'));
-app.use('/myBooking',requireCustAuth, require('./routes/customer/myBookings'));
+// app.use(custAuthRoutes)
+// app.use('/products', requireCustAuth, require('./routes/customer/products'));
+// app.use('/servicing', requireCustAuth, require('./routes/customer/servicing'));
+// app.use('/chatbot', requireCustAuth, require('./routes/customer/chatbotMsg'));
+// app.use('/myBooking',requireCustAuth, require('./routes/customer/myBookings'));
 
 
 
-// ! TESTING ROUTES
-app.use('/testing', require('./routes/customer/customer'))
-// app.get('*', checkCustUser)
+// // ! TESTING ROUTES
+// app.use('/testing', require('./routes/customer/customer'))
+// // app.get('*', checkCustUser)
 
-app.use('/', async (req, res) => {
-
-    return res.send("Successful");
-       
-});
 
 
 const path = require("path")
