cb7a09f60f0ba3105ca697846bf5ea1d53d5e8b0
tanaya123 <tanayawankar58@gmail.com>
2022-03-29 11:53:44 +0530

    Refractor files

--- app.js
+++ app.js
@@ -64,14 +64,13 @@ app.use('/testing', require('./routes/customer/customer'))
 
 const path = require("path")
 
-// ... other app.use middleware 
-app.use(express.static(path.join(__dirname, "client", "build")))
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static( 'client/build' ));
 
-// ...
-// Right before your app.listen(), add this:
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
+    });
+}
 
 app.listen(PORT, () => {
     console.log(`Server is running at PORT ${PORT}`);
