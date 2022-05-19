fa6bb674ea70d3f966b2166a3e9f332f80baf6fe
OmShende <omshende58@gmail.com>
2022-05-19 18:01:47 +0530

    Dynamic Test Drive

--- controller/outlet/testDriveController.js
+++ controller/outlet/testDriveController.js
@@ -1,5 +1,7 @@
 const testDriveSchema = require('../../models/outlet/testDriveSchema');
-
+const jwt = require('jsonwebtoken')
+const EVModel = require('../../models/common/ModelSchema');
+const Outlet = require('../../models/outlet/outletSchema');
 
 
 const testDrivesGet = (req,res,next) => {
@@ -9,9 +11,50 @@ const testDrivesGet = (req,res,next) => {
 }
 
 const testDrivePost = (req,res,next) => {
-    testDriveSchema.create(req.body).then(function(data){
-        res.send(data);
-    }).catch(next);
+    const token = req.cookies.jwttoken
+    //console.log(req.params.id)
+    if(!token)
+    {
+        console.log("No token")
+    }
+    jwt.verify(token, process.env.SECRET_KEY, async (err, decodedToken) => {
+        if (err) { //signatures dont match
+            console.log("error "+err)
+            res.send("You must be logged in to view this page")
+
+        }
+        else {
+            console.log(decodedToken.id)
+            console.log("||"+req.body.model+"||"+req.body.outlet)
+            Outlet.findOne({"name": req.body.outlet}).then((result)=>{
+                EVModel.findOne({"modelName": req.body.model}).then((result2)=>{
+                    console.log(result)
+                    console.log(result2)
+                    console.log(req.body.time+ "outlet" +req.body.outlet)
+
+                    const newTest = new testDriveSchema({
+                        customer: decodedToken.id,
+                        date: req.body.date, 
+                        time: req.body.time, 
+                        model: result2._id,
+                        address: req.body.address, 
+                        outlet: result._id,
+                        contact: req.body.contact,  
+                        pincode: req.body.pincode, 
+                        dob: req.body.dob, 
+                        name: req.body.name
+                        
+                    })
+                    newTest.save().then(() => {
+                        console.log("new test drive saved")
+                        return res.send("Booked")
+        
+                    })
+                })
+                
+            })
+            
+        }})
 }
 
 const testDrivePut = (req,res,next) => {
